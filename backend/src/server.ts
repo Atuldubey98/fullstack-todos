beb3d984b49564ef613bc2cd0b51b84081fc9c7d
Atuldubey98 <atuldubey017@gmail.com>
2023-05-03 16:09:51 +0530

    commit cluster

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,16 +1,27 @@
+import os from "os";
+import cluster from "cluster";
 import mongoose from "mongoose";
 import app from "./app";
 import config from "./config";
-
-async function setUpApplication() {
-  try {
-    await mongoose.connect(config.MONGO_URI);
-    app.listen(config.PORT, () => {
-      console.log("App is running on port ", config.PORT);
-    });
-  } catch (error) {
-    console.log(error);
+import http from "http";
+if (cluster.isPrimary) {
+  const nofCpus: number = os.cpus().length;
+  for (let i = 0; i < nofCpus; i++) {
+    cluster.fork();
   }
+} else {
+  async function setUpApplication() {
+    try {
+      await mongoose.connect(config.MONGO_URI);
+      const server = http.createServer(app);
+      server.listen(config.PORT, () => {
+        console.log(
+          `App is listening on port ${config.PORT} for pid ${process.pid}`
+        );
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  setUpApplication();
 }
-
-setUpApplication();
